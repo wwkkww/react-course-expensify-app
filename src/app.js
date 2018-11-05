fd25381fc9c6f6055bebcd44cc51f647649a6048
Kevin W <wongkokwah@gmail.com>
2018-11-05 21:23:04 +0800

    setup production webpack build

--- src/app.js
+++ src/app.js
@@ -16,15 +16,8 @@ store.dispatch(addExpense( {description: 'water bill', amount: 100, createdAt: -
 store.dispatch(addExpense( {description: 'gas bill', amount: 200, createdAt: 2000} ))
 store.dispatch(addExpense( {description: 'rent', amount: 150000, createdAt: 1000} ))
 
-// store.dispatch(setTextFilter('bill'));
-
-// setTimeout(() => {
-//   store.dispatch(setTextFilter('water'));
-// }, 3000)
-
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
 
 const jsx = (
   <Provider store={store}>
