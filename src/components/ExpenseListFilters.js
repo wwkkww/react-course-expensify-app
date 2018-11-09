6c7546321694c0ca2b368a04334f66c637bec49f
Kevin W <wongkokwah@gmail.com>
2018-11-09 23:28:35 +0800

    Add style

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -31,19 +31,28 @@ export class ExpenseListFilters extends React.Component {
   
   render() {
     return (
-        <div>
-          <input 
-            type="text" 
-            value={this.props.filters.text} 
-            onChange={this.onTextChange}
-          />
-          <select 
-            value={this.props.filters.sortBy}
-            onChange={this.onSortChange}>
-            <option value="date">Date</option>
-            <option value="amount">Amount</option>
-          </select>
-          <DateRangePicker 
+        <div className="content-container">
+          <div className="input-group">
+            <div className="input-group__item">
+              <input 
+                  type="text" 
+                  placeholder="Search expenses"
+                  className="text-input"
+                  value={this.props.filters.text} 
+                  onChange={this.onTextChange}
+              />
+            </div> 
+          <div className="input-group__item">
+            <select 
+              className="select"
+              value={this.props.filters.sortBy}
+              onChange={this.onSortChange}>
+              <option value="date">Date</option>
+              <option value="amount">Amount</option>
+            </select>
+          </div>
+          <div className="input-group__item">
+            <DateRangePicker 
               startDate={this.props.filters.startDate} 
               startDateId="startId" 
               endDate={this.props.filters.endDate} 
@@ -54,7 +63,9 @@ export class ExpenseListFilters extends React.Component {
               showClearDates={true}
               numberOfMonths={1}
               isOutsideRange={()=> false}
-          />
+            />
+          </div>
+          </div>
         </div>
     );
   }
